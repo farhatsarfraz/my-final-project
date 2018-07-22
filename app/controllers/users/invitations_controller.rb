eeef5c378386b9dafd817ab9f6ae1aa7745336c9
Maimoona <maimoonahaq84@gmail.com>
2018-07-22 18:32:13 +0500

    Delete created user, if email not sent

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -12,10 +12,12 @@ class Users::InvitationsController < Devise::InvitationsController
           if User.invite!(email: params[:user][:email], role: params[:user][:role])
             redirect_to root_path, notice: 'User has been invited successfully.'
           else
+            user = User.find_by(email: params[:user][:email])
+            user.destroy if user.present?
             redirect_to root_path, notice: 'Problem sending the inviataion to user: #{params[:user][:email]}.'
           end
         rescue
-          redirect_to root_path, notice: 'Problem sending the inviataion to user: #{params[:user][:email]}.'
+          redirect_to root_path, notice: "Problem sending the inviataion to user: #{params[:user][:email]}."
         end
       end
     end
