e903dc1c9ad24e9e40193ad80bceeddd1e84e837
farhatsarfraz23@outlook.com <farahsarfraz@gmail.com>
2018-08-03 11:19:27 +0500

    side navbar updated

--- db/migrate/20180719190129_add_name_to_route.rb
+++ db/migrate/20180719190129_add_name_to_route.rb
@@ -1,5 +1,5 @@
 class AddNameToRoute < ActiveRecord::Migration[5.1]
   def change
-    add_column :routes, :name, :string, null: false
+    add_column :routes, :name, :string, null: true
   end
 end
